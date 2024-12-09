693cb7797fd907d5b012cdb8ef62549246d5c096
AmitCauseWhyNot <amiteti77@gmail.com>
2024-12-09 13:23:02 +0200

    Added the MNIST dataset and a lib to work with it

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include "linear_algebra_stuff/matrix_stuff/matrix.h"
 #include "linear_algebra_stuff/vector_stuff/vector.h"
 #include "neural_network_stuff/neural_structions.h"
+#include "mnist.h"
 
 int main(int argc, char **argv)
 {
