d7a12395dfb93e27348e05b8305e371b36c08f19
AmitCauseWhyNot <amiteti77@gmail.com>
2024-12-09 07:30:03 +0200

    I finally added Gradient Descent (I fucking hate partial differantiation)

--- src/linear_algebra_stuff/vector_stuff/vector.c
+++ src/linear_algebra_stuff/vector_stuff/vector.c
@@ -5,24 +5,29 @@
 #include "../matrix_stuff/matrix.h"
 #include "vector.h"
 
-char* v_to_string(vector *v) {
+char *v_to_string(vector *v)
+{
     int buf_size = v->length * 10 + v->length * 2 + v->length / 10 * 2 + 20;
     char *result = malloc(buf_size);
-    if (result == NULL) {
+    if (result == NULL)
+    {
         return NULL;
     }
 
     char *ptr = result;
     ptr += sprintf(ptr, "[\n   Length: %u,\n   [", v->length);
 
-    for (Index i = 0; i < v->length; i++) {
+    for (Index i = 0; i < v->length; i++)
+    {
         ptr += sprintf(ptr, "%.4f", v->values[i]);
 
-        if (i < v->length - 1) {
+        if (i < v->length - 1)
+        {
             ptr += sprintf(ptr, ", ");
         }
-        
-        if ((i + 1) % 10 == 0 && i < v->length - 1) {
+
+        if ((i + 1) % 10 == 0 && i < v->length - 1)
+        {
             ptr += sprintf(ptr, "\n    ");
         }
     }
@@ -32,82 +37,170 @@ char* v_to_string(vector *v) {
     return result;
 }
 
-double* r_scale(double *row, double scale, Index size) {
+double *r_scale(double *row, double scale, Index size)
+{
     double *r_return = malloc(size * sizeof(double));
-    
-    for (int i = 0; i < size; i++) {
+
+    for (int i = 0; i < size; i++)
+    {
         r_return[i] = row[i] * scale;
     }
 
     return r_return;
 }
 
-double r_sum(double *row, Index size) {
+double r_sum(double *row, Index size)
+{
     double sum;
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         sum += row[i];
     }
 
     return sum;
 }
 
-vector* v_create(Index length, double *values) {
-    vector* v_return = malloc(sizeof(vector));
+vector *v_create(Index length, double *values)
+{
+    vector *v_return = malloc(sizeof(vector));
 
-    if (v_return == NULL) {
+    if (v_return == NULL)
+    {
         return NULL;
     }
 
     v_return->length = length;
     v_return->values = malloc(length * sizeof(double));
+    for (int i = 0; i < length; i++)
+    {
+        v_return->values[i] = 0.0;
+    }
 
-    if (v_return->values == NULL) {
+    if (v_return->values == NULL)
+    {
         return NULL;
     }
 
-    if (values != NULL) {
+    if (values != NULL)
+    {
         v_return->values = values;
     }
 
     return v_return;
 }
 
-vector* v_scale(vector *v, double scale) {
+vector *v_scale(vector *v, double scale)
+{
     vector *v_return = v_create(v->length, NULL);
 
-    for (int i = 0; i < v_return->length; i++) {
+    for (int i = 0; i < v_return->length; i++)
+    {
         v_return->values[i] = v->values[i] * scale;
     }
 
     return v_return;
 }
 
-vector* v_add(vector *v1, vector *v2) {
-    if (v1->length != v2->length) {
+vector *v_add(vector *v1, vector *v2)
+{
+    if (v1->length != v2->length)
+    {
         return NULL;
     }
 
     vector *v_return = v_create(v1->length, NULL);
 
-    for (int i = 0; i < v_return->length; i++) {
+    for (int i = 0; i < v_return->length; i++)
+    {
         v_return->values[i] = v1->values[i] + v2->values[i];
     }
 
     return v_return;
 }
 
-vector* m_v_mult(matrix* m, vector* v) {
-    if (m->Nrows != v->length) {
+vector *v_sub(vector *v1, vector *v2)
+{
+    if (v1->length != v2->length)
+    {
         return NULL;
     }
-    
+
+    vector *v_return = v_create(v1->length, NULL);
+
+    for (int i = 0; i < v_return->length; i++)
+    {
+        v_return->values[i] = v1->values[i] - v2->values[i];
+    }
+
+    return v_return;
+}
+
+vector *m_v_mult(matrix *m, vector *v)
+{
+    if (m->Ncols != v->length)
+    {
+        return NULL;
+    }
+
     vector *v_return = v_create(v->length, NULL);
 
-    for (int i = 0; i < v->length; i++) {
+    for (int i = 0; i < v->length; i++)
+    {
         v_return->values[i] = m_dot(m->values[i], v->values, v->length);
     }
 
     return v_return;
 }
 
+vector *v_m_weighted_sum(matrix *m, vector *v)
+{
+    if (m->Ncols != v->length)
+    {
+        return NULL;
+    }
+
+    vector *v_return = v_create(m->Nrows, NULL);
+
+    for (int i = 0; i < m->Nrows; i++)
+    {
+        for (int j = 0; j < m->Ncols; j++)
+        {
+            v_return->values[i] += m->values[i][j] * v->values[j];
+        }
+    }
+
+    return v_return;
+}
+
+vector *H_product(vector *v1, vector *v2)
+{
+    if (v1->length != v2->length)
+    {
+        return NULL;
+    }
+
+    vector *v_return = v_create(v1->length, NULL);
+
+    for (int i = 0; i < v_return->length; i++)
+    {
+        v_return->values[i] = v1->values[i] * v2->values[i];
+    }
+
+    return v_return;
+}
+
+matrix *v_vT_mult(vector *v1, vector *v2)
+{
+    matrix *m_return = m_create(v1->length, v2->length);
+
+    for (int i = 0; i < m_return->Nrows; i++)
+    {
+        for (int j = 0; j < m_return->Ncols; j++)
+        {
+            m_return->values[i][j] = v1->values[i] * v2->values[j];
+        }
+    }
+
+    return m_return;
+}
--- src/neural_network_stuff/neural_structions.c
+++ src/neural_network_stuff/neural_structions.c
@@ -6,14 +6,41 @@
 #include "../linear_algebra_stuff/vector_stuff/vector.h"
 #include "neural_structions.h"
 
-double sigmoid(double value) {
+double sigmoid(double value)
+{
     return 1.0 / (1 + exp(value));
 }
 
-double next_neuron_value(Layer* l, Layer* n_l, int index) {
+double d_sigmoid(double value)
+{
+    return sigmoid(value) * (1 - sigmoid(value));
+}
+
+double softmax(vector *v)
+{
+    vector *v_return = v_create(v->length, NULL);
+
+    double exp_sum = 0.0;
+
+    for (int i = 0; i < v->length; i++)
+    {
+        exp_sum += exp(v->values[i]);
+    }
+
+    for (int i = 0; i < v_return->length; i++)
+    {
+        v_return->values[i] = exp(v->values[i]) / exp_sum;
+    }
+
+    return v_return;
+}
+
+double next_neuron_value(Layer *l, Layer *n_l, int index)
+{
     double sum_values = 0.0;
-    
-    for (int i = 0; i < l->size; i++) {
+
+    for (int i = 0; i < l->size; i++)
+    {
         sum_values += (l->neurons[i].weights[index] * l->neurons[i].value);
         // printf("Index: %d, This Value: %f, Weight: %f, Value: %f\n", i, sum_values, l->neurons[i].weights[i] * l->neurons[i].value);
     }
@@ -23,10 +50,70 @@ double next_neuron_value(Layer* l, Layer* n_l, int index) {
     return sigmoid(sum_values);
 }
 
-vector* v_sigmoid(vector *v) {
+double loss_function(vector *prediction, double *real)
+{
+    double total_loss = 0, value;
+
+    for (int i = 0; i < prediction->length; i++)
+    {
+        value = (real[i] - prediction->values[i]);
+        total_loss += (value * value);
+    }
+
+    return total_loss / (prediction->length * 2);
+}
+
+matrix *get_weight_gradient(vector *L, vector *A)
+{
+    return v_vT_mult(L, A);
+}
+
+vector *get_bias_gradient(vector *L)
+{
+    return L;
+}
+
+vector *get_hidden_lambda(matrix *W, vector *L, (double)(*a_f)(double), vector *Z)
+{
+    W = m_transpose(W);
+    Z = v_d_sigmoid(Z);
+
+    vector *l_return = v_create(W->Nrows, NULL);
+    vector *W_L_mult = m_v_mult(W, L);
+
+    l_return = H_product(W_L_mult, Z);
+
+    return l_return;
+}
+
+vector *get_output_lambda(vector *A, vector *Y)
+{
+    if (A->length != Y->length)
+    {
+        return NULL;
+    }
+
+    return v_sub(A, Y);
+}
+
+vector *v_d_sigmoid(vector *v)
+{
+    vector *v_return = v_create(v->length);
+
+    for (int i = 0; i < v_return->length)
+    {
+        v_return->values[i] = d_sigmoid(v->values[i]);
+    }
+
+    return v_return;
+}
+
+vector *v_sigmoid(vector *v)
+{
     vector *v_return = v_create(v->length, NULL);
 
-    for (int i = 0; i < v_return->length; i++) {
+    for (int i = 0; i < v_return->length; i++)
+    {
         v_return->values[i] = sigmoid(v->values[i]);
     }
 
