26bfbd8554f1184f0c0bfa732703a023c7b4cf70
AmitCauseWhyNot <amiteti77@gmail.com>
2025-01-01 10:38:00 +0200

    Just updated the mnist.h

--- src/MNIST_stuff/mnist.h
+++ src/MNIST_stuff/mnist.h
@@ -1,3 +1,6 @@
+#ifndef MNIST_H
+#define MNIST_H
+
 #include <stdio.h>
 #include <stdint.h>
 
@@ -6,3 +9,5 @@ uint32_t convert_to_32int(uint8_t *buff);
 double *get_image(char *path, int num);
 
 int get_label(char *path, int num);
+
+#endif
